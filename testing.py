b919ea893f82f4ddd8c7fe3e0e9ee6a6be3c7592
sameermeher <sam.tiku@gmail.com>
2025-08-15 17:53:46 +0530

    first commit

--- testing.py
+++ testing.py
@@ -21,6 +21,14 @@ def user_update(user_id:int, user:User):
     else:
         return {"message": "User not found"}
 
+@app.put("/user_db/data/v2/update/{user_id}")
+def user_update(user_id:int, user:User):
+    if user_id in user_db:
+        user_db[user_id] = user.dict()
+        return {"message": "User updated successfully", "user": user_db[user_id]}
+    else:
+        return {"message": "User not found"}
+
 @app.delete("/user_db/data/v1/delete/{user_id}")
 def delete_user(user_id:int):
     if user_id in user_db:
